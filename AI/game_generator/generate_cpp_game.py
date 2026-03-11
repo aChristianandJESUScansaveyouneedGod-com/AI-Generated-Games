f866f6fe95054905d50be04f48b18107a4dc6ef7
aChristianandJESUScansaveyouneedGod-com <justusherrin@gmail.com>
2026-03-10 17:02:10 -0700

    Create generate_cpp_game.py

---
+++ AI/game_generator/generate_cpp_game.py
@@ -0,0 +1,15 @@
+def generate_cpp_template():
+
+    code = """
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "AI Generated Game Running" << std::endl;
+    return 0;
+}
+
+"""
+
+    return code
